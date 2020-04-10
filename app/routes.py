cfeb0d47cdf6f1bf420c94c21462a7da8a2d22dc
rach62 <raja.asokan@gmail.com>
2020-04-10 16:03:32 +0200

    created templates folder

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    return render_template('index.html')
+
+
+@app.route('/image')
+def image():
+    return "Raja Asokan is a good boy!"
