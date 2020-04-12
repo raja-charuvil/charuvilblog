b2ae78b4ec18e0568ade94d26802f8282fb732ef
rach62 <raja.asokan@gmail.com>
2020-04-12 11:13:41 +0200

    completed basic struture

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,38 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 
-@app.route('/image')
-def image():
-    return "Raja Asokan is a good boy!"
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/books')
+def books():
+	return render_template('books.html')
+
+
+@app.route('/images')
+def images():
+    return render_template('images.html')
+
+
+@app.route('/articles')
+def articles():
+	return render_template('articles.html')
+
+
+@app.route('/socialmedia')
+def socialmedia():
+	return render_template('socialmedia.html')
+
+
+@app.route('/others')
+def others():
+	return render_template('others.html')
